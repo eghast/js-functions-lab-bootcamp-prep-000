a9c7cd3a919794cb6d4a35e46c96076ae0964e27
Elizabeth Gholson <eliz.gholson.9@gmail.com>
2018-05-07 19:25:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,6 @@ function happyHolidayTo(holiday, name){
   return "Happy Independence Day, you!"
 }
 
-function holidayCountdown(holiday, days)
+function holidayCountdown(holiday, days){
+  return "It\'s 20 days "
+}
